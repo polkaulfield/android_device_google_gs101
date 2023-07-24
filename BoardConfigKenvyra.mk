cfd8e8d41016ae550ef200b72e1a4019c6ee55be
Pol Kaulfield <mr.tamuraa@gmail.com>
2023-07-24 22:26:44 +0200

    Kenvyra Bringup

--- BoardConfigKenvyra.mk
+++ BoardConfigKenvyra.mk
@@ -21,7 +21,7 @@ TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_DEADLINE := true
 TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE := false
 
 # Manifests
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/kenvyra/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += device/google/gs101/manifest_radio.xml
 
 # Partitions
